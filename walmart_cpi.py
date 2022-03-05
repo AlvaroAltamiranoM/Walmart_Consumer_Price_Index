0b7808c62cd9b5f8a5bf127d95e601eab2cc3154
Alvaro Altamirano Montoya <48501231+AlvaroAltamiranoM@users.noreply.github.com>
2022-03-05 09:17:47 -0500

    Added countries' loop

--- walmart_cpi.py
+++ walmart_cpi.py
@@ -6,7 +6,7 @@ Created on Sun Jan  2 17:03:48 2022
 """
 import os
 
-path = r'C:\Users\unily\OneDrive\Documentos\Python Scripts\walmart\data'
+path = r'root'
 os.chdir(path)
 
 
@@ -43,149 +43,141 @@ headers = {
     'accept-language': 'en-US,en;q=0.9,es-MX;q=0.8,es;q=0.7'
 }
 
-'''
-URL = 'https://www.walmart.com.ni/abarrotes'
-page = requests.get(URL)
-# specifying a desired format of "page" using the html parser    soup = BeautifulSoup(page.text, "html.parser")
-soup = BeautifulSoup(page.text, "html.parser")
-Ofertas_Activas = soup.find(
-    class_="vtex-search-result-3-x-totalProducts--layout pv5 ph9 bn-ns bt-s b--muted-5 tc-s tl t-action--small").text
-Ofertas_Activas = int(''.join(filter(str.isdigit, Ofertas_Activas)))
-print(Ofertas_Activas)
-'''
-
 #Empty list
 texto = []
+countries = ['sv','gt','mx']
+section = ['abarrotes','higiene-y-belleza','farmacia', 'articulos-para-el-hogar']
 #items_perpage = 21
 #for pages in range(1, int((Ofertas_Activas / items_perpage) + 1)):
-for pages in range(1,51): #50 parece ser el max de paginas por seccion
-    URL = 'https://www.walmart.com.ni/abarrotes?order=OrderByTopSaleDESC&page='+format(pages)
-    print(URL)
-    # conducting a request of the stated URL above:
-    driver.get(URL)
-#Esperando que la información esté visible y ejecutando crawler:
-    time.sleep(5)
-    try:
-        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(10)
-        element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[1]/div/div[5]/div/div/section/div[2]/div/div/div/div[4]/div/div[2]/div/div[6]/div/div/div/div[2]/div[24]'))
-        WebDriverWait(driver, 30).until(element_present)
-        results = [i.text for i in driver.find_elements_by_xpath('/html/body/div[2]/div/div[1]/div/div[5]/div/div/section/div[2]/div/div/div/div[4]/div/div[2]/div/div[6]/div/div/div/div[2]/div')]            
-        print(results)
+for cc in countries:
+    for pages in range(1,50): #50 parece ser el max de paginas por seccion
+        for sec in section:
+            URL = 'https://www.walmart.com.{0}/'.format(cc)+format(sec)+'?order=OrderByTopSaleDESC&page='+format(pages)
+            print(URL)
+            # conducting a request of the stated URL above:
+            driver.get(URL)
+        #Esperando que la información esté visible y ejecutando crawler:
+            time.sleep(5)
+            try:
+                driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+                time.sleep(10)
+                element_present = EC.presence_of_element_located((By.XPATH, '/html/body/div[2]/div/div[1]/div/div[5]/div/div/section/div[2]/div/div/div/div[4]/div/div[2]/div/div[6]/div/div/div/div[2]/div[24]'))
+                WebDriverWait(driver, 30).until(element_present)
+                results = [i.text for i in driver.find_elements_by_xpath('/html/body/div[2]/div/div[1]/div/div[5]/div/div/section/div[2]/div/div/div/div[4]/div/div[2]/div/div[6]/div/div/div/div[2]/div')]            
+                print(results)
+                texto.append(results)
+                time.sleep(1)
+            except:
+                break
+            pass
         texto.append(results)
-        time.sleep(1)
-    except:
-        break
-    pass
+            
+        
+    #Procesando data descargada
+    # Manipulacion de listas para crear dataframe
+    listas = [i for sublist in texto for i in sublist]            
+    data = pd.DataFrame(listas)
+    data = data[0].str.split('\n', expand=True).dropna(thresh=3).drop_duplicates([0])
+    #Creando variables
+    data['price'] = data[1].str.extract('(\d+)').astype('int')
+    data['name'] = data[0].str.lower()
+    data['weight_grams'] = data['name'].str.\
+        extract(r'((\d+(?=gr))|(\d+(?=\sgr))|(\d+(?=grs))|(\d+(?=\sgrs))|(\d+(?=\sg))|(\d+(?=g)))')[0]
     
+    data['weight_lbs'] = data['name'].str.\
+        extract(r'((\d+(?=lb))|(\d+(?=\slb))|(\d+(?=lbs))|(\d+(?=\slbs))|(\d+(?=\slb))|(\d+(?=lb)))')[0]
+    data['weight_ml'] = data['name'].str.\
+        extract(r'((\d+(?=ml))|(\d+(?=\sml))|(\d+(?=mls))|(\d+(?=\smls))|(\d+(?=\sml))|(\d+(?=ml)))')[0]
     
-#Procesando data descargada
-# Manipulacion de listas para crear dataframe
-listas = [i for sublist in texto for i in sublist]            
-data = pd.DataFrame(listas)
-data = data[0].str.split('\n', expand=True).dropna(thresh=3).drop_duplicates([0])
-#Creando variables
-data['price'] = data[1].str.extract('(\d+)').astype('int')
-data['name'] = data[0].str.lower()
-data['weight_grams'] = data['name'].str.\
-    extract(r'((\d+(?=gr))|(\d+(?=\sgr))|(\d+(?=grs))|(\d+(?=\sgrs))|(\d+(?=\sg))|(\d+(?=g)))')[0]
-
-data['weight_lbs'] = data['name'].str.\
-    extract(r'((\d+(?=lb))|(\d+(?=\slb))|(\d+(?=lbs))|(\d+(?=\slbs))|(\d+(?=\slb))|(\d+(?=lb)))')[0]
-data['weight_ml'] = data['name'].str.\
-    extract(r'((\d+(?=ml))|(\d+(?=\sml))|(\d+(?=mls))|(\d+(?=\smls))|(\d+(?=\sml))|(\d+(?=ml)))')[0]
-
-data['weight'] = data['weight_grams'].astype(float)/1000
-data['weight_lb_to_kg'] = data['weight_lbs'].astype(float)/2.20462
-data['weight_ml_to_kg'] = data['weight_ml'].astype(float)/1000
-data['weight'] = data['weight'].fillna(data['weight_ml_to_kg'])
-data['weight'] = data['weight'].fillna(data['weight_lb_to_kg'])
-data['weight'] = data['weight'].round(2)
-
-data['name'] = data['name'].str.replace('\d+', '')
-data['name'] = data['name'].str.replace('- gr', '')
-data['name'] = data['name'].str.replace(' gr', '')
-data['name'] = data['name'].str.replace('- ml', '')
-data['name'] = data['name'].str.replace(' ml', '')
-data['name'] = data['name'].str.replace('-', '')
-data['name'] = data['name'].str.replace('lb', '')
-
-data['country'] = 'nic'
-data['date'] = date.today()
-
-data = data[['name','weight','price','country','date']]
-data = data.drop_duplicates(['name'])
-
-## GRAPHS
-# Graph 1
-fig1 = px.bar(data[0:15].sort_values('price'), x='price', y="name", 
-             template="simple_white", text='price',
-             title= 'Fig 1. Top Selling Grocery Items (sorted by unit price)')
-
-fig1.update_layout(showlegend=True, legend_title_text='',
-                xaxis_title="Price in Local Currency Units (nominal C$)", 
-                yaxis_title="",
-                font_family="Arial", title_font_family="Arial Black",
-                yaxis_title_font_family ="Arial Black",
-                xaxis_title_font_family ="Arial Black",
-                title_font_color="black",
-                title_font_size=19, legend_title_font_color="black",
-                legend=dict(yanchor="top", y=0.89, xanchor="right", x=1),
-                )
-fig1.update_traces(textposition='inside')
-fig1.add_annotation(
-    text="Source: author.",
-    xref="x domain", yref="y domain",x=0.01, y=-0.14,showarrow=False)
-
-plot(fig1)
-
-fig2 = px.bar(data.sort_values('weight', ascending=False)[0:15], x='weight', y="name", 
-             template="simple_white", text='weight',
-             title= 'Fig 2. Heaviest Grocery Items')
-
-fig2.update_layout(showlegend=True, legend_title_text='',
-                xaxis_title="Weight in Kilograms", 
-                yaxis_title="",
-                font_family="Arial", title_font_family="Arial Black",
-                yaxis_title_font_family ="Arial Black",
-                xaxis_title_font_family ="Arial Black",
-                title_font_color="black",
-                title_font_size=19, legend_title_font_color="black",
-                legend=dict(yanchor="top", y=0.89, xanchor="right", x=1),
-                )
-fig2.update_traces(textposition='inside')
-fig2.add_annotation(
-    text="Source: author.",
-    xref="x domain", yref="y domain",x=0.01, y=-0.14,showarrow=False)
-
-plot(fig2)
-
-fig3 = px.bar(data.sort_values('price', ascending=False)[0:15], x='price', y="name", 
-             template="simple_white", text='price',
-             title= 'Fig 3. Most Expensive Single Grocery Items')
-
-fig3.update_layout(showlegend=True, legend_title_text='',
-                xaxis_title="Price in Local Currency Units (nominal C$)", 
-                yaxis_title="",
-                font_family="Arial", title_font_family="Arial Black",
-                yaxis_title_font_family ="Arial Black",
-                xaxis_title_font_family ="Arial Black",
-                title_font_color="black",
-                title_font_size=19, legend_title_font_color="black",
-                legend=dict(yanchor="top", y=0.89, xanchor="right", x=1),
-                )
-fig3.update_traces(textposition='inside')
-fig3.add_annotation(
-    text="Source: author.",
-    xref="x domain", yref="y domain",x=0.01, y=-0.14,showarrow=False)
-
-plot(fig3)
-
-#Salvando serie
-#data.to_csv(r'walmart_{0}.csv'.format(date.today()), index = False)
-
-baseline_date = date.today() - timedelta(days=6)
-
-history = pd.read_csv(r'walmart_{0}.csv'.format(baseline_date))
-
-history.append(data, sort = True, ignore_index=True).to_csv(r'walmart_{0}.csv'.format(date.today()), index = False)
+    data['weight'] = data['weight_grams'].astype(float)/1000
+    data['weight_lb_to_kg'] = data['weight_lbs'].astype(float)/2.20462
+    data['weight_ml_to_kg'] = data['weight_ml'].astype(float)/1000
+    data['weight'] = data['weight'].fillna(data['weight_ml_to_kg'])
+    data['weight'] = data['weight'].fillna(data['weight_lb_to_kg'])
+    data['weight'] = data['weight'].round(2)
+    
+    data['name'] = data['name'].str.replace('\d+', '')
+    data['name'] = data['name'].str.replace('- gr', '')
+    data['name'] = data['name'].str.replace(' gr', '')
+    data['name'] = data['name'].str.replace('- ml', '')
+    data['name'] = data['name'].str.replace(' ml', '')
+    data['name'] = data['name'].str.replace('-', '')
+    data['name'] = data['name'].str.replace('lb', '')
+    
+    data['country'] = cc
+    data['date'] = date.today()
+    
+    data = data[['name','weight','price','country','date']]
+    data = data.drop_duplicates(['name'])
+    '''
+    ## GRAPHS
+    # Graph 1
+    fig1 = px.bar(data[0:15].sort_values('price'), x='price', y="name", 
+                 template="simple_white", text='price',
+                 title= 'Fig 1. Top Selling Grocery Items (sorted by unit price)')
+    
+    fig1.update_layout(showlegend=True, legend_title_text='',
+                    xaxis_title="Price in Local Currency Units (nominal C$)", 
+                    yaxis_title="",
+                    font_family="Arial", title_font_family="Arial Black",
+                    yaxis_title_font_family ="Arial Black",
+                    xaxis_title_font_family ="Arial Black",
+                    title_font_color="black",
+                    title_font_size=19, legend_title_font_color="black",
+                    legend=dict(yanchor="top", y=0.89, xanchor="right", x=1),
+                    )
+    fig1.update_traces(textposition='inside')
+    fig1.add_annotation(
+        text="Source: author.",
+        xref="x domain", yref="y domain",x=0.01, y=-0.14,showarrow=False)
+    
+    plot(fig1)
+    
+    fig2 = px.bar(data.sort_values('weight', ascending=False)[0:15], x='weight', y="name", 
+                 template="simple_white", text='weight',
+                 title= 'Fig 2. Heaviest Grocery Items')
+    
+    fig2.update_layout(showlegend=True, legend_title_text='',
+                    xaxis_title="Weight in Kilograms", 
+                    yaxis_title="",
+                    font_family="Arial", title_font_family="Arial Black",
+                    yaxis_title_font_family ="Arial Black",
+                    xaxis_title_font_family ="Arial Black",
+                    title_font_color="black",
+                    title_font_size=19, legend_title_font_color="black",
+                    legend=dict(yanchor="top", y=0.89, xanchor="right", x=1),
+                    )
+    fig2.update_traces(textposition='inside')
+    fig2.add_annotation(
+        text="Source: author.",
+        xref="x domain", yref="y domain",x=0.01, y=-0.14,showarrow=False)
+    
+    plot(fig2)
+    
+    fig3 = px.bar(data.sort_values('price', ascending=False)[0:15], x='price', y="name", 
+                 template="simple_white", text='price',
+                 title= 'Fig 3. Most Expensive Single Grocery Items')
+    
+    fig3.update_layout(showlegend=True, legend_title_text='',
+                    xaxis_title="Price in Local Currency Units (nominal C$)", 
+                    yaxis_title="",
+                    font_family="Arial", title_font_family="Arial Black",
+                    yaxis_title_font_family ="Arial Black",
+                    xaxis_title_font_family ="Arial Black",
+                    title_font_color="black",
+                    title_font_size=19, legend_title_font_color="black",
+                    legend=dict(yanchor="top", y=0.89, xanchor="right", x=1),
+                    )
+    fig3.update_traces(textposition='inside')
+    fig3.add_annotation(
+        text="Source: author.",
+        xref="x domain", yref="y domain",x=0.01, y=-0.14,showarrow=False)
+    
+    plot(fig3)
+    '''
+    #Salvando serie
+    #data.to_csv(r'walmart_{0}_{1}.csv'.format(cc,date.today()), index = False)  
+    
+    baseline_date = date.today() - timedelta(days=1)
+    history = pd.read_csv(r'walmart_{0}_{1}.csv'.format(cc, baseline_date))
+    history.append(data, sort = True, ignore_index=True).to_csv(r'walmart_{0}_{1}.csv'.format(cc,date.today()), index = False)
